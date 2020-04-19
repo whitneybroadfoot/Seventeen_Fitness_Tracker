8bc6a4848aa23cc836acafd9c237ff4b1cbb3c8f
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-19 10:38:01 -0600

    updating serverjs

--- server.js
+++ server.js
@@ -17,37 +17,37 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 });
 
 // routes
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/exercise", function(req, res) {
+app.get("/exercise", function (req, res) {
 	res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
-app.get("/stats", function(req, res) {
+app.get("/stats", function (req, res) {
 	res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
-app.post("/api/workouts", function(req, res) {
+app.post("/api/workouts", function (req, res) {
 	db.create(req.body).then(data => res.json(data));
 });
 
-app.get("/api/workouts", function(req, res) {
+app.get("/api/workouts", function (req, res) {
 	db.find().then(results => {
 		console.log(results[results.length - 1]);
 		res.json(results[results.length - 1]);
 	});
 });
 
-app.get("/api/workouts/range", function(req, res) {
+app.get("/api/workouts/range", function (req, res) {
 	db.find().then(data => {
 		console.log(data);
 		res.json(data);
 	});
 });
 
-app.put("/api/workouts/:id", function(req, res) {
+app.put("/api/workouts/:id", function (req, res) {
 	console.log(req.body);
 	db.update(
 		{ _id: req.params.id },
